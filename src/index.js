ecac3d595d56fb3873c05c3c4f9b1950fc1e91d9
João Pedro <117098725+joao-dev7@users.noreply.github.com>
2023-08-07 10:57:56 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const radio = document.getElementsByName("avaliacao");
+const botao = document.querySelector(".botao");
+const p = document.getElementById("valorEnviado");
+
+document.getElementById(`label4`).style.background = "hsl(216, 12%, 54%";
+p.innerHTML = `You selected 5 out of 5`;
+
+function ValorAnterior() {
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      colocarCor(i);
+      p.innerHTML = `You selected ${i + 1} out of 5`;
+    } else {
+      tirarCor(i);
+    }
+  }
+}
+
+botao.onclick = function clickBotao(e) {
+  e.preventDefault();
+  document.querySelector(".caixa1").style.display = "none";
+  document.querySelector(".caixa2").style.display = "flex";
+};
+
+function colocarCor(i) {
+  if (radio[i].value > 1) {
+    document.getElementById(`label${radio[i].value - 1}`).style.background = "hsl(216, 12%, 54%";
+    }
+}
+
+function tirarCor(i) {
+  document.getElementById(`label${radio[i].value}`).style.background =
+    "hsl(215, 9%, 26%)";
+}
